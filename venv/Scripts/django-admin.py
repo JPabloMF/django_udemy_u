2e89a220d305d39161ebc53c98b0835b13d4a8b4
JpabloMF <juanpablo45671@gmail.com>
2020-05-17 22:17:33 -0500

    initial django project

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!E:\Desarrollo\PROGRAMMING COURSES\PYTHON\DJANGO\people_django\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
